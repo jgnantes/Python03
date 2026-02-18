d7ea01682a1d2f2b7f36077aa0d08ead64d4da4a
jgnantes <jgnantes@outlook.com>
2026-02-18 01:18:20 -0300

    ex6 quase feito, falta sets

--- ex6/ft_analytics_dashboard.py
+++ ex6/ft_analytics_dashboard.py
@@ -4,38 +4,55 @@ if __name__ == "__main__":
     print("=== List Comprehension Examples ===\n")
     players: list = ['João', 'Pepita', 'Carol de Niterói', 'Inbonha',
     'Tulla Luana', 'Wanessa Wolf (são elas)']
-    scores: list = [8000, 42, 123, 22002, 1, 123]
-    if len(players) == len(scores):
-        print("Final Scores:")
-        i: int = 0
-        for player in players:
-            print(f"{i + 1} - {player}: {sorted(scores, reverse = True)[i]}")
-            i += 1
+    scores: list = [8000, 42, 123, 22002, 1, 122]
+    lowests = [score for score in scores if score < 123]
+    print(f"Lowest scores: {sorted(lowests)}")
+    highests = [score for score in scores if score >= 123]
+    print(f"Highest scores: {sorted(highests)}")
+    doubled = [score*2 for score in scores]
+    print(f"Douled scores: {doubled}")
+    girlies = [name for name in players if name[len(name) - 1] == 'a']
+    print(f"Girliest players: {girlies}")
     print("")
 
     print("=== Dict Comprehension Examples ===\n")
-    scoreboard: dict = {}
+    scores_sort = sorted(scores, reverse = True)
     if len(players) == len(scores):
-        j: int = 0
-        for player in players:
-            scoreboard[player] = sorted(scores, reverse = True)[j]
-            j += 1
-        print("The Same Scoreboard from Before but Now with Dicts")
-        k: int = 0
+        scoreboard = {players[i]:scores_sort[i] for i in range(len(players))}
+        print("Scoreboard:")
+        i: int = 0
         for item in scoreboard:
-            print(f"{k + 1} - {item}: {scoreboard[item]} (with dicts)")
-            k += 1
-    print("")
+            print(f"{i + 1} - {item}: {scoreboard[item]}")
+            i += 1
+    categories = {
+        players[i]: "highest" if scores[i] >= 123 else "lowest"
+        for i in range(len(players))
+    }
+    print("\nBest ones:", end = ' ')
+    for player in categories:
+        if categories[player] == "highest":
+            print(f"{player}", end = ', ')
+    print("and that's about it")
+    print("Shame on them:", end = ' ')
+    for player in categories:
+        if categories[player] == "lowest":
+            print(f"{player}", end = ', ')
+    print("no one else\n")
 
     print("=== Set Comprehension Examples ===\n")
-    print("You Guessed It")
-    player_set = set(players)
 
     print("=== Combined Analysis ===\n")
-    print("\nSome Random Stuff")
-    highest = max(scores)
-    print(f"- {players[0]} has the highest score: {highest} points")
-    lowest = min(scores)
-    print(
-        f"- {players[len(players) - 1]} has the lowest: {lowest} point(s)"
-        )
+    print("Some Random Stuff")
+    if len(players) == len(scores):
+        highest = max(scores)
+        print(f"- {players[0]} has the highest score: {highest} points")
+        lowest = min(scores)
+        print(
+            f"- {players[len(players) - 1]} has the lowest: {lowest} point(s)"
+            )
+    print(f"- Total players: {len(players)}")
+    sum_scores: int = 0
+    for score in scores:
+        sum_scores += score
+    print(f"- Average score: {sum_scores / len(scores):.2f}")
+
